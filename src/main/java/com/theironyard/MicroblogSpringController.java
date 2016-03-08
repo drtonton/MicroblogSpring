d4188280a4c507d57e6a0119ef3720b785a00300
drtonton <tkfoster26@gmail.com>
2016-03-08 14:55:18 -0500

    complete

--- src/main/java/com/theironyard/MicroblogSpringController.java
+++ src/main/java/com/theironyard/MicroblogSpringController.java
@@ -1,23 +1,43 @@
 package com.theironyard;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by noellemachin on 3/7/16.
  */
+@Controller
 public class MicroblogSpringController {
+    ArrayList<String> messages = new ArrayList<>();
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model, HttpSession session) {
+    public String home(Model model, HttpSession session) { // httpsession wants a GD object for some reason.
         model.addAttribute("name", session.getAttribute("userName"));
+        model.addAttribute("messages", messages);
         return "home";
+    }
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(HttpSession session, String userName) {
+        session.setAttribute("userName", new User(userName));
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/createMessage", method = RequestMethod.POST)
+    public String createMessage(HttpSession session, String message) {
+        session.getAttribute("userName");
+        messages.add(message);
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    public String delete(HttpSession session, String message) {
+        session.getAttribute("username");
+        String messageToRemove = message;
+        messages.remove(messageToRemove);
+        return "redirect:/";
     }
 }
 
-//    In your controller, create a route for /
-//        It should take the model and the request as arguments
-//        It should read the username from the session and add it to the model
-//        It should return the home template
---
+++ src/main/java/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by noellemachin on 3/8/16.
+ */
+public class User {
+    String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+}
